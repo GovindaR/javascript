f797d594d3805eae7d064926b05b6de14001c56b
Govinda <vegarana44@gmail.com>
2018-09-12 08:28:40 +0545

    html and js code maintenance

--- js/main.js
+++ js/main.js
@@ -1,28 +1,35 @@
 
+// array Map,Filter & Sum
 
-var numberString = ['1','2','3','4','20', '23', '22'];
+let numberString = ['1','2','3','4','20', '23', '22'];
+
 function getSumOfEvenDigits(data){
-    return data
-    .map(function(item,index){
 
-        console.log(+item * 5);
+    return data
+
+    .map(function(item){
 
         return +item * 5;
     })
 
-    .filter(function(item,index){
-        return item % 2 ===0;
+    .filter(function(item){
+
+        return item % 2 === 0;
     })
     .reduce(function(total,item){
+
         return total + item;
     })
     
 }
+
+
+// Sum of array
+
 function getColorText(){
-    let para = document.getElementById('paragraph');
+    let para = document.getElementById('myhtml-para');
+
     para.style.color='blue';
-    para.innerHTML='js';
-    document.getElementById('result').innerHTML='Result:'+ getSumOfEvenDigits(numberString);
+    para.innerHTML='paragraph';
+    document.getElementById('result').innerHTML='Result :'+' '+ getSumOfEvenDigits(numberString);
 }
-// result value
-// p tag ma text change
